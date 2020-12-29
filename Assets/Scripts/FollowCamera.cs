25afde3c3bf1d2abd3462ec8f23552a208aabeb2
David Spak <david.spak478@gmail.com>
2020-12-29 23:33:49 +0100

    Move on click, follow camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // set camera position equals to target (player) position
+        transform.position = target.position;
+    }
+}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,6 +10,25 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if(Input.GetMouseButton(0)) {
+            MoveToCursor();
+        }
+
+        // GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        // show click target
+        // Debug.DrawRay(ray.origin, ray.direction * 100);
+
+        // if ray intersects with a collider => move to click target
+        if(hasHit) {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
