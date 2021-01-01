d6b678d63eec4094fb62eba3f68592497f8d050d
David Spak <david.spak478@gmail.com>
2021-01-01 23:34:27 +0100

    Added attack animation

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,23 +7,40 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
         Transform target;
+        float timeSinceLastAttack = 0;
 
-        void Update() {
+        void Update()
+        {
+            // time since last update
+            timeSinceLastAttack += Time.deltaTime;
+
             // check if target is set
             if (target != null) {
-                // check if distance between player and target is less than weaponRange
+                // check if distance between player and target is less than weapon range
                 bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
 
                 // if player is not in range, then move to target
                 if(!isInRange) {
                     GetComponent<Mover>().MoveTo(target.position);
-                } else {
+                } else
+                {
                     GetComponent<Mover>().Cancel();
+                    AttackBehaviour();
                 }
             }
         }
-        
+
+        private void AttackBehaviour()
+        {
+            // check time between atacks and run attack animation
+            if (timeSinceLastAttack >= timeBetweenAttacks) {
+                timeSinceLastAttack = 0;
+                GetComponent<Animator>().SetTrigger("attack");
+            }
+        }
+
         public void Attack(CombatTarget combatTarget)
         {
             // start attack action
@@ -34,6 +51,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        // Animation Event
+        private void Hit()
+        {
+
         }
     }
 }
